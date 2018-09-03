0c9748f99229eac7247d90471e130ec545efc9fb
LucentW <LucentW@users.noreply.github.com>
2018-09-03 21:02:51 +0200

    Allow nested boost groups
    Halium won't load the underlying LXC container without that...

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -666,12 +666,6 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 	if (!parent_css)
 		return &root_schedtune.css;
 
-	/* Allow only single level hierachies */
-	if (parent_css != &root_schedtune.css) {
-		pr_err("Nested SchedTune boosting groups not allowed\n");
-		return ERR_PTR(-ENOMEM);
-	}
-
 	/* Allow only a limited number of boosting groups */
 	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
 		if (!allocated_group[idx])
